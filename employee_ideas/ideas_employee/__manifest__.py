2351e6c18a44b0567abc043c4d7df031b8ac8129
luckyramdani <lucky.ramdani.tif16@polban.ac.id>
2018-04-15 11:38:42 +0700

    employee ideas

---
+++ employee_ideas/ideas_employee/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name':'Employee Ideas',
+    'description':'Manage your Employee Ideas',
+    'author':'Lucky Ramdani',
+    'depends':['base'],
+    'application': True,
+    'data':[
+        'view/ideas_view.xml',
+		'view/ideas_menu.xml',
+        ],
+}
