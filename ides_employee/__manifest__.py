c599a3c0b3c15f6ceaeae95027a8f011b4d3cf3d
luckyramdani <lucky.ramdani.tif16@polban.ac.id>
2018-05-20 11:52:19 +0700

    implementasi sidebar

---
+++ ides_employee/__manifest__.py
@@ -0,0 +1,8 @@
+{
+	'name': 'Sale Forecast',
+	'description': '.',
+	'author': 'A3 Proyek4',
+	'depends': ['base', 'mail'],
+	'application': True,
+	'data': ['views/ideas_view.xml', 'views/employee_ideas_menu.xml', 'views/ideas_types.xml'],
+}
