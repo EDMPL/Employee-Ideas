46cf4150a9801e3010f86204489741bac5323740
Faatih Syauqi <faatihsyauqi@gmail.com>
2018-05-22 12:03:28 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/EDMPL/Project-4""
    This reverts commit 13de94b96dfdc28c6eca8b3dc7cd470b248efffe.

--- financial_forecast/__manifest__.py
+++ financial_forecast/__manifest__.py
@@ -5,6 +5,11 @@
 	'depends':['base'],
 	'application': True,
 	'data': [
+		'reports/receivable_report.xml',
+		'reports/others_report.xml',
+		'reports/payable_report.xml',
+		'views/others_view.xml',
+		'views/payable_view.xml', 
 		'views/receivable_view.xml', 
 		'views/menu_view.xml'],
 }
