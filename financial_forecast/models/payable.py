71215788e817b381322bde68c2bd9ed9dfc9d686
luckyramdani <lucky.ramdani.tif16@polban.ac.id>
2018-06-01 07:38:47 +0700

    add keteragann field

--- financial_forecast/models/payable.py
+++ financial_forecast/models/payable.py
@@ -6,6 +6,7 @@ class Payable(models.Model):
 	
 	nama = fields.Char('Nama', required=True)
 	kategori = fields.Many2one('category.forecast', 'Category')
+	keterangan = fields.Char('Keterangan', required=True)
 	tanggal_peminjaman = fields.Date('Tanggal Peminjaman')
 	tenggat_waktu = fields.Date('Tenggat Waktu', required=True)
 	total_biaya  = fields.Integer('Total Biaya')
