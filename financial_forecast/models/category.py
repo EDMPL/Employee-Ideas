a11098545f0f99912bce5d0c277948518d5439c4
luckyramdani <lucky.ramdani.tif16@polban.ac.id>
2018-06-26 06:00:45 +0700

    translate to eng and add total amount report
    translate to eng and add total amount report

--- financial_forecast/models/category.py
+++ financial_forecast/models/category.py
@@ -3,5 +3,5 @@ from odoo import models, fields, api
 class CashCategory(models.Model):
 	_name = 'category.forecast'
 	_description = 'Cash Flow Category'
-	name	= fields.Char(string='Nama Kategori', required=True)
+	name	= fields.Char(string='Category Name', required=True)
     
